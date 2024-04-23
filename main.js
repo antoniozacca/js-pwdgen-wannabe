a17360671a1e465405723b9a43e0e25e61a0ef69
antoniozacca <antoniozaccaria057@gmail.com>
2024-04-23 14:52:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let nome= prompt ("Qual è il tuo nome?")
+console.log(nome)
+
+let cognome= prompt ("Qual è il tuo cognome?")
+console.log(cognome)
+
+let colorePreferito= prompt ("Qual è il tuo colore preferito?")
+console.log(colorePreferito)
+
+document.write(nome + cognome + colorePreferito + "21")
+
